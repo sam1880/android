dbb53a1410a26c61116a93b99fc663075a40e3ae
sam1880 <50699518+sam1880@users.noreply.github.com>
2022-02-01 15:06:31 +0530

    Create diceRoller.kt

---
+++ diceRoller.kt
@@ -0,0 +1,26 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text="let's roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = java.util.Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+
+    }
+}
